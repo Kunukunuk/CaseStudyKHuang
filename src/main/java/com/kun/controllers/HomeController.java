8f381decb89fd52d882fd4d1591d8813748932f9
Kun Huang <kkun.huang@gmail.com>
2019-11-01 20:05:05 -0400

    setting up servlet, maven dependencies, and views folder for jsp

---
+++ src/main/java/com/kun/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.kun.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public ModelAndView getHome() {
+        ModelAndView mav = new ModelAndView("home");
+        System.out.println("I am here*****");
+        return mav;
+    }
+
+}
--- web/views/jsp/home.jsp
+++ web/views/jsp/home.jsp
@@ -2,15 +2,15 @@
   Created by IntelliJ IDEA.
   User: kunhuang
   Date: 11/1/19
-  Time: 11:05 AM
+  Time: 11:37 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Home Tab</title>
+</head>
+<body>
+This is the home
+</body>
 </html>
